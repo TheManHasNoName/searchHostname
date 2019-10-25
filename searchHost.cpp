c8ee0307ea0fdc9d20fdcec29a3db58e597ef546
Elnas <Xavier.Elnas@spi-global.com>
2019-10-26 01:07:43 +0800

    revert due to bugs

--- searchHost.cpp
+++ searchHost.cpp
@@ -11,24 +11,18 @@ int main (){
 	string text;
 	ifstream file("hostnames.csv");
 	
-	do {
-			cout << "Enter SD/Long Hostname: ";
-			getline (cin, sd);
+
+	cout << "Enter SD/Long Hostname: ";
+	getline (cin, sd);
 			
-			while(!file.eof()){
-				getline(file,text);
+	while(!file.eof()){
+		getline(file,text);
 				
-				if (text.find(sd) != string::npos) {
-					cout << text;
-				} else {
-					cout << "Unable to search SD/Hostname. ";
-					break;
-				}
-			}
-			
-			cout << "\nType no to exit otherwise, repeat: ";
-			getline (cin, exit);
-		} while (exit != "no");
-		
+		if (text.find(sd) != string::npos) {
+			cout << text;
+			break;
+		}
+	}
+	
 	return 0;
 }
